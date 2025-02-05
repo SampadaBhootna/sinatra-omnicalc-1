b0596b0ad2234710bd7012aa8646de1a24682ea5
SampadaBhootna <sbhootna@chicagobooth.edu>
2025-02-05 16:37:54 +0000

    final app

--- app.rb
+++ app.rb
@@ -15,7 +15,57 @@ get("/square/new") do
 end
 
 get("/square/results") do
+  @the_num=params.fetch("number").to_f
+  @the_result=@the_num**2
 
   erb(:square_results)
 
+end
+
+#Square Root Calculation#
+
+get("/square_root/new") do
+
+  erb(:square_root_calc)
+
+end
+
+get("/square_root/results") do
+
+  @the_num=params.fetch("user_number").to_f
+  @the_result=@the_num**0.5
+
+  erb(:square_root_results)
+end
+
+#EMI Payment calculcator#
+get("/payment/new") do
+
+  erb(:payment_calc)
+
+end
+
+get("/payment/results") do
+  @I=params.fetch("user_apr").to_f
+  @Y=params.fetch("user_years").to_f 
+  @P=params.fetch("user_pv").to_f
+
+  @the_result= (@P*@I*(1+@I)**(@Y*12))/((1+@I)**(@Y*12)-1)
+
+  erb(:payment_results)
+
+end
+
+#Pick Random Number#
+get("/random/new") do
+  erb(:random_num_calc)
+end
+
+get("/random/results") do
+  @the_max=params.fetch("user_max").to_f
+  @the_min=params.fetch("user_min").to_f
+
+  @the_result=rand(@the_min..@the_max)
+
+  erb(:random_results)
 end
