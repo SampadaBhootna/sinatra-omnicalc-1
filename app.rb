f59329d3464d615a9c5fcc68b3677490dabefa55
SampadaBhootna <sbhootna@chicagobooth.edu>
2025-02-05 16:37:30 +0000

    final app

--- app.rb
+++ app.rb
@@ -7,3 +7,15 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+    
+    erb(:square_new)
+    
+end
+
+get("/square/results") do
+
+  erb(:square_results)
+
+end
